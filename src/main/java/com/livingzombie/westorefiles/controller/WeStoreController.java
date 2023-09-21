cb11641c161fa4d8108aa101a85d69ea5cba6d76
pravendrakhichi <khinchi.1@iitj.ac.in>
2023-09-21 17:26:00 +0530

    added controller

--- src/main/java/com/livingzombie/westorefiles/controller/WeStoreController.java
+++ src/main/java/com/livingzombie/westorefiles/controller/WeStoreController.java
@@ -1,7 +1,17 @@
 package com.livingzombie.westorefiles.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.http.HttpResponse;
 
 @Controller
 public class WeStoreController {
+
+    @RequestMapping("/")
+    public ResponseEntity<String> helloWorld(){
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
