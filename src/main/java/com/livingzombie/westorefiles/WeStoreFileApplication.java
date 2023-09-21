6f2b28c7743b72033a458bdb777db1b5f47d44cf
pravendrakhichi <khinchi.1@iitj.ac.in>
2023-09-21 16:38:17 +0530

    added WeStoreFileApplication application

---
+++ src/main/java/com/livingzombie/westorefiles/WeStoreFileApplication.java
@@ -0,0 +1,13 @@
+package com.livingzombie.westorefiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeStoreFileApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello this is file storage system");
+        SpringApplication.run(WeStoreFileApplication.class);
+    }
+}
